16d4e3c87ed892255ab15bcb196914396f3527ab
gaoxing12 <1570275769@qq.com>
2019-05-02 17:42:50 +0800

    第三页的刷新页面

--- js/information.js
+++ js/information.js
@@ -1,6 +1,13 @@
-//	var informTitleA = document.getElementsByClassName("inform_title_a");
-//	for(var i=0;i>informTitleA.length-1;i++){
-//		informTitleA.onclick=function(){
-//			informTitleA[i].
-//		}
-//	}
+	var informTitleA = document.getElementsByClassName("inform_title_a");
+	informTitleA[0].onclick=function(){
+//		console.log(this);
+//		console.log(informTitleA[1]);
+		informTitleA[0].className="inform_title_a informH_active";
+		informTitleA[1].className="inform_title_a";
+	}
+	informTitleA[1].onclick=function(){
+//		console.log(this);
+//		console.log(informTitleA[1]);
+		informTitleA[1].className="inform_title_a informH_active";
+		informTitleA[0].className="inform_title_a";
+	}
